2d149e3fee742e4ed8bbbe0e7f17a7db459666ab
valdasivanauskas <valdas.ivanauskas.1966@gmail.com>
2020-12-24 14:40:52 +0200

    level

--- topics.py
+++ topics.py
@@ -1,7 +1,8 @@
+from collections import defaultdict
 from f_excel.c_excel import Excel
 
 
-class Topics(list):
+class Topics(defaultdict):
 
     # Row of the First-Topic
     fr = 1
@@ -9,16 +10,25 @@ class Topics(list):
     fc = 1
 
     def __init__(self, path_myq):
-        super().__init__()
+        super().__init__(defaultdict)
+        self.__load(path_myq)
 
     def __load(self, path_myq):
         xlsx = path_myq + 'topics.xlsx'
         excel = Excel(xlsx)
         row = self.fr
         while not excel.is_blank(row, self.fc):
-            name = excel.get_value(row, self.fc)
-            priority = excel.get_value(row, self.fc+1)
-
-
+            col = self.fc
+            while not excel.is_blank(row, col):
+                name = excel.get_value(row, col)
+                priority = excel.get_value(row, col+1)
+                if priority in {'A', 'B', 'C'}:
+                    self[name] = priority
+                col += 1
+            row += 1
         excel.close()
+
+
+t = Topics(path_myq='d:\\myq\\')
+print(t)
 
