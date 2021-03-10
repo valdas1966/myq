3adadbbfd1227a961da1ebb51533953f7d1a6997
valdasivanauskas <valdas.ivanauskas.1966@gmail.com>
2021-03-10 18:07:38 +0200

    dg

--- consoles/auto_fill.py
+++ consoles/auto_fill.py
@@ -5,83 +5,46 @@ from f_excel.c_excel import Excel
 from f_utils import u_datetime
 
 
-def fill_id():
+def auto_fill():
     """
     ============================================================================
-     Description: Auto-Fill Id-Column with Incremental Integer.
+     Description: AutoFill Excel-Questions Columns (Id, Valid, Priority, Date).
     ============================================================================
     """
-    col_id = 1
     row_first = 2
+    col_id = 1
+    col_valid = 2
+    col_priority = 3
+    col_date = 7
+    date = u_datetime.to_str(datetime.now(), 'yymmdd')
     id_last = params.get('id last')
     filepaths = u_excel.get_filepaths_questions()
     for xlsx in filepaths:
+        changed = False
         excel = Excel(xlsx)
         row_last = excel.ws.max_row
         for row in range(row_first, row_last+1):
+            # AutoFill Id
             if excel.is_blank(row, col_id):
-                excel.set_value(row, col_id, id_last+1)
+                excel.set_value(row, col_id, id_last + 1)
                 id_last += 1
-        excel.close()
-    params.set('id last', id_last)
-        
-
-def fill_valid():
-    """
-    ============================================================================
-     Description: Auto-Fill Valid-Column with 1 Values (Is Valid).
-    ============================================================================
-    """
-    col_valid = 2
-    row_first = 2
-    filepaths = u_excel.get_filepaths_questions()
-    for xlsx in filepaths:
-        excel = Excel(xlsx)
-        row_last = excel.ws.max_row
-        for row in range(row_first, row_last+1):
+                changed = True
+            # AutoFill Valid
             if excel.is_blank(row, col_valid):
                 excel.set_value(row, col_valid, 1)
-        excel.close()
-
-
-def fill_priority():
-    """
-    ============================================================================
-     Description: Auto-Fill Priority-Column with 'A' Value.
-    ============================================================================
-    """
-    col_priority = 1
-    row_first = 2
-    filepaths = u_excel.get_filepaths_questions()
-    for xlsx in filepaths:
-        excel = Excel(xlsx)
-        row_last = excel.ws.max_row
-        for row in range(row_first, row_last + 1):
+                changed = True
+            # AutoFill Priority
             if excel.is_blank(row, col_priority):
                 excel.set_value(row, col_priority, 'A')
-        excel.close()
-
-
-def fill_date():
-    """
-    ============================================================================
-     Description: Auto-Fill Priority-Column with 'A' Value.
-    ============================================================================
-    """
-    col_date = 7
-    row_first = 2
-    date = u_datetime.to_str(datetime.now(), 'yymmdd')
-    filepaths = u_excel.get_filepaths_questions()
-    for xlsx in filepaths:
-        excel = Excel(xlsx)
-        row_last = excel.ws.max_row
-        for row in range(row_first, row_last + 1):
+                changed = True
+            # AutoFill Date
             if excel.is_blank(row, col_date):
                 excel.set_value(row, col_date, date)
+                changed = True
         excel.close()
+        if changed:
+            print(xlsx)
+    params.set('id last', id_last)
 
 
-fill_id()
-fill_valid()
-fill_priority()
-fill_date()
+auto_fill()
