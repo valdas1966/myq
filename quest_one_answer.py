b079070933f35605b72babf4b270aa049147e2ba
valdasivanauskas <valdas.ivanauskas.1966@gmail.com>
2021-06-20 14:31:45 +0300

    aa

--- quest_one_answer.py
+++ quest_one_answer.py
@@ -27,6 +27,8 @@ class QuestOneAnswer(Quest):
         """
         super().ask(counter, repeated)
         timer = Timer()
+        if '\n' in self.text:
+            print(self.text)
         self.ans = input(self.text + '-> ')
         # True-Answer
         if self.ans == self.ans_true:
