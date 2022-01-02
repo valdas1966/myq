ef0737cc3f68aeff57c84a8e11fc38144023045a
valdasivanauskas <valdas.ivanauskas.1966@gmail.com>
2022-01-02 14:20:18 +0200

    a

--- consoles/cn_change_xlsx.py
+++ consoles/cn_change_xlsx.py
@@ -1,5 +1,6 @@
 from f_utils import u_file
 from f_excel.c_excel import Excel
+import re
 
 repo = 'd:\\myq\\questions'
 col_q = 4
@@ -12,20 +13,6 @@ for xlsx in u_file.filepaths(repo, extensions='xlsx'):
     for r in range(fr, lr+1):
         q = str(excel.get_value(r, col_q))
         a = str(excel.get_value(r, col_a))
-        size = 25
-        if ' ' in a:
-            continue
-        if '*'*size not in q:
-            continue
-        i = q.find('*' * size)
-        if i+size < len(q):
-            if 'a' <= q[i+size] <= 'z':
-                continue
-        sub = q[max(0,i-1):i+size]
-        print(len(a)-int(sub[0] != ' '), )
-        sub_new = sub[0] + '*'*(len(a)-int(sub[0] != ' '))
-        print(q)
-        q = q.replace(sub, sub_new)
-        print(q)
-        excel.set_value(r, col_q, q)
+        q_new = re.sub(r'\*{6,}', '*****', q)
+        excel.set_value(r, col_q, q_new)
     excel.close()
